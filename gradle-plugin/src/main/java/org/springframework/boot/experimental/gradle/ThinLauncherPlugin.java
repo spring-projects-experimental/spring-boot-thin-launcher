f26f6dd8ae31e93342666c5db8ca156cd2fa435f
Andy Wilkinson <awilkinson@pivotal.io>
2016-10-31 15:49:00 +0000

    Basic Gradle plugin to generate the META-INF/lib.properties file

---
+++ gradle-plugin/src/main/java/org/springframework/boot/experimental/gradle/ThinLauncherPlugin.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2012-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.experimental.gradle;
+
+import java.io.File;
+
+import org.gradle.api.Action;
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+import org.gradle.api.plugins.JavaPlugin;
+import org.gradle.api.plugins.JavaPluginConvention;
+import org.gradle.api.tasks.SourceSetContainer;
+import org.gradle.api.tasks.TaskContainer;
+
+/**
+ * Gradle {@link Plugin} for Spring Boot's thin launcher.
+ * <p>
+ * If the Java plugin is applied to the project, a {@link GenerateLauncherPropertiesTask}
+ * named {@code generateLibProperties} is added to the project. This task is configured to
+ * use the {@code runtime} configuration to generate a {@code META-INF/lib.properties}
+ * file in the {@code main} source set's resource output directory.
+ *
+ * @author Andy Wilkinson
+ */
+public class ThinLauncherPlugin implements Plugin<Project> {
+
+	public void apply(final Project target) {
+		target.getPlugins().withType(JavaPlugin.class, new Action<JavaPlugin>() {
+
+			@Override
+			public void execute(JavaPlugin plugin) {
+				createLibPropertiesTask(target);
+			}
+
+		});
+	}
+
+	private void createLibPropertiesTask(final Project project) {
+		TaskContainer taskContainer = project.getTasks();
+		taskContainer.create("generateLibProperties", GenerateLauncherPropertiesTask.class, new Action<GenerateLauncherPropertiesTask>() {
+
+				@Override
+				public void execute(GenerateLauncherPropertiesTask libPropertiesTask) {
+					configureLibPropertiesTask(libPropertiesTask, project);
+				}
+
+		});
+
+	}
+
+	private void configureLibPropertiesTask(GenerateLauncherPropertiesTask libPropertiesTask, Project project) {
+		libPropertiesTask.setConfiguration(project.getConfigurations()
+				.getByName(JavaPlugin.RUNTIME_CONFIGURATION_NAME));
+		SourceSetContainer sourceSets = project.getConvention()
+				.getPlugin(JavaPluginConvention.class).getSourceSets();
+		File resourcesDir = sourceSets.getByName("main").getOutput().getResourcesDir();
+		libPropertiesTask.setOutput(new File(resourcesDir, "META-INF/lib.properties"));
+		project.getTasks().getByName(JavaPlugin.JAR_TASK_NAME).dependsOn(libPropertiesTask);
+	}
+
+}
