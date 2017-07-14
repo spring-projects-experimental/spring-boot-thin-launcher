f88245753ce28dc181c9d36eeb1abdeb37cf9526
punishme <punishme@users.noreply.github.com>
2017-07-14 08:10:25 +0200

    Added missing import.

--- launcher/src/main/java/org/springframework/boot/loader/thin/MavenSettings.java
+++ launcher/src/main/java/org/springframework/boot/loader/thin/MavenSettings.java
@@ -26,6 +26,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
 
 import org.apache.maven.model.ActivationFile;
 import org.apache.maven.model.ActivationOS;
