2b043c61e0dd3e26e3b7a027f6fa6f46e6c8f62a
Dave Syer <dsyer@pivotal.io>
2017-09-12 11:58:48 +0100

    Extract classpath computation into standalone tool

---
+++ tools/classpath/src/main/java/org/springframework/boot/loader/thin/classpath/LogUtils.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2016-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.boot.loader.thin.classpath;
+
+import java.security.CodeSource;
+import java.security.ProtectionDomain;
+
+import org.slf4j.ILoggerFactory;
+import org.slf4j.impl.StaticLoggerBinder;
+
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+
+/**
+ * @author Dave Syer
+ *
+ */
+class LogUtils {
+
+	public static void setLogLevel(Level level) {
+		setLogLevel(Logger.ROOT_LOGGER_NAME, level);
+	}
+
+	private static void setLogLevel(String loggerName, Level level) {
+		ch.qos.logback.classic.Logger logger = getLogger(loggerName);
+		if (logger != null) {
+			logger.setLevel(level);
+		}
+	}
+
+	private static ch.qos.logback.classic.Logger getLogger(String name) {
+		LoggerContext factory = getLoggerContext();
+		if (StringUtils.isEmpty(name)) {
+			name = Logger.ROOT_LOGGER_NAME;
+		}
+		return factory.getLogger(name);
+
+	}
+
+	private static LoggerContext getLoggerContext() {
+		ILoggerFactory factory = StaticLoggerBinder.getSingleton().getLoggerFactory();
+		Assert.isInstanceOf(LoggerContext.class, factory,
+				String.format(
+						"LoggerFactory is not a Logback LoggerContext but Logback is on "
+								+ "the classpath. Either remove Logback or the competing "
+								+ "implementation (%s loaded from %s). If you are using "
+								+ "WebLogic you will need to add 'org.slf4j' to "
+								+ "prefer-application-packages in WEB-INF/weblogic.xml",
+						factory.getClass(), getLocation(factory)));
+		return (LoggerContext) factory;
+	}
+
+	private static Object getLocation(ILoggerFactory factory) {
+		try {
+			ProtectionDomain protectionDomain = factory.getClass().getProtectionDomain();
+			CodeSource codeSource = protectionDomain.getCodeSource();
+			if (codeSource != null) {
+				return codeSource.getLocation();
+			}
+		}
+		catch (SecurityException ex) {
+			// Unable to determine location
+		}
+		return "unknown location";
+	}
+
+}
---
+++ tools/classpath/src/main/java/org/springframework/boot/loader/thin/classpath/ThinClasspathApplication.java
@@ -0,0 +1,176 @@
+/*
+ * Copyright 2012-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.loader.thin.classpath;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.loader.archive.Archive;
+import org.springframework.boot.loader.thin.ThinJarLauncher;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.SimpleCommandLinePropertySource;
+import org.springframework.core.env.StandardEnvironment;
+
+import ch.qos.logback.classic.Level;
+
+/**
+ * Utility for printing classpath in various formats.
+ * 
+ * @author Dave Syer
+ *
+ */
+public class ThinClasspathApplication {
+
+	private static final Logger log = LoggerFactory
+			.getLogger(ThinClasspathApplication.class);
+
+	private static final String THIN_SOURCE = "thin.source";
+
+	private StandardEnvironment environment = new StandardEnvironment();
+
+	public static void main(String[] args) throws Exception {
+		LogUtils.setLogLevel(Level.OFF);
+		new ThinClasspathApplication(args).print();
+	}
+
+	private String[] args;
+
+	private ThinClasspathApplication(String[] args) {
+		addCommandLineProperties(args);
+		this.args = args;
+	}
+
+	private void addCommandLineProperties(String[] args) {
+		if (args == null || args.length == 0) {
+			return;
+		}
+		MutablePropertySources properties = environment.getPropertySources();
+		SimpleCommandLinePropertySource source = new SimpleCommandLinePropertySource(
+				"commandArgs", args);
+		if (!properties.contains("commandArgs")) {
+			properties.addFirst(source);
+		}
+		else {
+			properties.replace("commandArgs", source);
+		}
+	}
+
+	private void print() throws Exception {
+		File jarfile = new File(findSource());
+		if (!jarfile.exists()) {
+			System.err.println(
+					"Archive does not exist or is not a file (try running with java -jar): "
+							+ jarfile);
+			return;
+		}
+		if (needsHelp(this.args, jarfile)) {
+			help();
+			return;
+		}
+		boolean trace = !"false"
+				.equals(environment.resolvePlaceholders("${thin.trace:${trace:false}}"));
+		if (trace) {
+			LogUtils.setLogLevel(Level.DEBUG);
+		}
+		System.setProperty(ThinJarLauncher.THIN_ARCHIVE, jarfile.getAbsolutePath());
+		new ClasspathLauncher(args).print();
+	}
+
+	private String findSource() {
+		String source = environment.getProperty(THIN_SOURCE);
+		if (source == null) {
+			source = System.getProperty("java.class.path");
+			String separator = System.getProperty("path.separator");
+			if (source.contains(separator)) {
+				source = source.substring(source.indexOf(separator));
+			}
+		}
+		if (source.startsWith("file:")) {
+			source = source.substring("file:".length(), source.length());
+			if (source.startsWith("//")) {
+				source = source.substring(2);
+			}
+		}
+		return source;
+	}
+
+	private void help() {
+		System.out.println(
+				"\nPrints the classpath for a thin executable jar or exploded archive.\n\n"
+						+ "Usage: run a thin jar and make this one the --thin.library for it. E.g. \n\n"
+						+ "    $ java -jar myapp.jar --thin.library=org.springframework.boot.experimental:spring-boot-thin-classpath:1.0.7.BUILD-SNAPSHOT\n");
+	}
+
+	private boolean needsHelp(String[] args, File jarfile) {
+		Set<String> strings = new HashSet<>(Arrays.asList(args));
+		try {
+			return strings.contains("--help") || strings.contains("-h")
+					|| jarfile.toURI().toURL().equals(getClass().getProtectionDomain()
+							.getCodeSource().getLocation());
+		}
+		catch (MalformedURLException e) {
+			return true;
+		}
+	}
+
+	private static class ClasspathLauncher extends ThinJarLauncher {
+
+		protected ClasspathLauncher(String[] args) throws Exception {
+			super(args);
+		}
+
+		public void print() throws Exception {
+			System.out.println(classpath(getClassPathArchives()));
+		}
+
+		private String classpath(List<Archive> archives) throws Exception {
+			StringBuilder builder = new StringBuilder();
+			String separator = System.getProperty("path.separator");
+			boolean first = true;
+			for (Archive archive : archives) {
+				if (!first) {
+					first = false;
+					continue;
+				}
+				if (builder.length() > 0) {
+					builder.append(separator);
+				}
+				log.info("Archive: {}", archive);
+				String uri = archive.getUrl().toURI().toString();
+				if (uri.startsWith("jar:")) {
+					uri = uri.substring("jar:".length());
+				}
+				if (uri.startsWith("file:")) {
+					uri = uri.substring("file:".length());
+				}
+				if (uri.endsWith("!/")) {
+					uri = uri.substring(0, uri.length() - "!/".length());
+				}
+				builder.append(new File(uri).getCanonicalPath());
+			}
+			return builder.toString();
+		}
+
+	}
+}
