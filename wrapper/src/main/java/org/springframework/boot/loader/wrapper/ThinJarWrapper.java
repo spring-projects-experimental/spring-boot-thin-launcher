4cb93c6cd24bb190310a139ccca3ddf7627f0951
Dave Syer <dsyer@pivotal.io>
2018-09-06 15:13:41 +0100

    Revert to snapshots

--- wrapper/src/main/java/org/springframework/boot/loader/wrapper/ThinJarWrapper.java
+++ wrapper/src/main/java/org/springframework/boot/loader/wrapper/ThinJarWrapper.java
@@ -90,7 +90,7 @@ public class ThinJarWrapper {
 
 	private static final String DEFAULT_LAUNCHER_CLASS = "org.springframework.boot.loader.thin.ThinJarLauncher";
 
-	private static final String DEFAULT_LIBRARY = "org.springframework.boot.experimental:spring-boot-thin-launcher:jar:exec:1.0.15.RELEASE";
+	private static final String DEFAULT_LIBRARY = "org.springframework.boot.experimental:spring-boot-thin-launcher:jar:exec:1.0.16.BUILD-SNAPSHOT";
 
 	private Properties properties;
 
