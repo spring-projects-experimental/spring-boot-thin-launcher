1241629994d17f540850c91c576b8ebc261d61ae
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2018-10-03 13:38:21 +0100

    Updates to support Java 11

--- samples/tests/src/test/java/com/example/AppMavenIT.java
+++ samples/tests/src/test/java/com/example/AppMavenIT.java
@@ -123,7 +123,7 @@ public class AppMavenIT {
 		started = builder.start();
 		String output = output(started.getInputStream(), "Started LauncherApplication");
 		assertThat(output).contains("Jetty started");
-		assertThat(output).contains("1.4.1.RELEASE");
+		assertThat(output).contains("2.0.5.RELEASE");
 	}
 
 	private static String output(InputStream inputStream, String marker)
