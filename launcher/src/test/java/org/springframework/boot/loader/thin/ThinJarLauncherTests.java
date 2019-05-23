6db64ca8770f155d9cb61e9ea93fc60737dd07ab
Dave Syer <dsyer@pivotal.io>
2019-05-23 08:18:37 +0200

    Refactor test for settings.xml repository location setting
    The jitpack repo is now mirrored in repo.spring.io so we can't rely
    on non-existence of those artifacts to show that they are not being
    configured properly.

--- launcher/src/test/java/org/springframework/boot/loader/thin/ThinJarLauncherTests.java
+++ launcher/src/test/java/org/springframework/boot/loader/thin/ThinJarLauncherTests.java
@@ -16,12 +16,14 @@
 package org.springframework.boot.loader.thin;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.charset.Charset;
 import java.util.Collections;
 import java.util.Properties;
 
 import org.eclipse.aether.artifact.DefaultArtifact;
 import org.eclipse.aether.graph.Dependency;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -30,6 +32,8 @@ import org.mockito.ArgumentCaptor;
 import org.springframework.boot.test.rule.OutputCapture;
 import org.springframework.core.io.Resource;
 import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.util.FileSystemUtils;
+import org.springframework.util.StreamUtils;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Matchers.any;
@@ -377,28 +381,39 @@ public class ThinJarLauncherTests {
 	}
 
 	@Test
-	@Ignore("Not sure why this broke. Local settings?")
 	public void repositorySettingsMissing() throws Exception {
 		DependencyResolver.close();
-		deleteRecursively(new File("target/thin/test/repository/com/github/jitpack"));
+		deleteRecursively(new File("target/thin/test/repository/com/example"));
 		String[] args = new String[] { "--thin.root=target/thin/test",
 				"--thin.dryrun=true", "--thin.archive=src/test/resources/apps/jitpack",
 				"--debug" };
 		expected.expect(RuntimeException.class);
-		expected.expectMessage("maven-simple:jar:1.1");
+		expected.expectMessage("maven-simple:jar:1.0");
 		ThinJarLauncher.main(args);
-		assertThat(new File("target/thin/test/repository/com/github/jitpack/maven-simple")
+		assertThat(new File("target/thin/test/repository/com/example/maven/maven-simple")
 				.exists()).isFalse();
 	}
 
 	@Test
 	public void repositorySettingsPresent() throws Exception {
 		DependencyResolver.close();
+		File userhome = new File("target/settings/repo/.m2");
+		if (!userhome.exists()) {
+			userhome.mkdirs();
+		}
+		String settings = StreamUtils.copyToString(
+				new FileInputStream(
+						new File("src/test/resources/settings/repo/.m2/settings.xml")),
+				Charset.defaultCharset());
+		settings = settings.replace("${repo.url}",
+				"file://" + new File("target/test-classes/repo").getAbsolutePath());
+		StreamUtils.copy(settings, Charset.defaultCharset(),
+				new FileOutputStream(new File(userhome, "settings.xml")));
 		String home = System.getProperty("user.home");
 		System.setProperty("user.home",
-				new File("src/test/resources/settings/profile").getAbsolutePath());
+				new File("target/settings/repo").getAbsolutePath());
 		try {
-			deleteRecursively(new File("target/thin/test/repository/com/github/jitpack"));
+			deleteRecursively(new File("target/thin/test/repository/com/example"));
 			String[] args = new String[] { "--thin.root=target/thin/test",
 					"--thin.dryrun=true",
 					"--thin.archive=src/test/resources/apps/jitpack", "--debug" };
@@ -408,7 +423,7 @@ public class ThinJarLauncherTests {
 			System.setProperty("user.home", home);
 		}
 		assertThat(new File("target/thin/test/repository").exists()).isTrue();
-		assertThat(new File("target/thin/test/repository/com/github/jitpack/maven-simple")
+		assertThat(new File("target/thin/test/repository/com/example/maven/maven-simple")
 				.exists()).isTrue();
 	}
 
@@ -433,23 +448,7 @@ public class ThinJarLauncherTests {
 	}
 
 	public static boolean deleteRecursively(File root) {
-		if (root != null && root.exists()) {
-			if (root.isDirectory()) {
-				File[] children = root.listFiles();
-				if (children != null) {
-					for (File child : children) {
-						deleteRecursively(child);
-					}
-				}
-			}
-			boolean deleted = root.delete();
-			if (!deleted) {
-				System.err.println("Cannot delete: " + root);
-			}
-			return deleted;
-		}
-		System.err.println("Did not delete: " + root);
-		return false;
+		return FileSystemUtils.deleteRecursively(root);
 	}
 
 }
