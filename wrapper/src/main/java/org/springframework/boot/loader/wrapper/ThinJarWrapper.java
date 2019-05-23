962ed318a2040810706925882cd0cc7b9f4c8378
Dave Syer <dsyer@pivotal.io>
2019-05-23 06:50:09 +0200

    Release 1.0.22

--- wrapper/src/main/java/org/springframework/boot/loader/wrapper/ThinJarWrapper.java
+++ wrapper/src/main/java/org/springframework/boot/loader/wrapper/ThinJarWrapper.java
@@ -90,7 +90,7 @@ public class ThinJarWrapper {
 
 	private static final String DEFAULT_LAUNCHER_CLASS = "org.springframework.boot.loader.thin.ThinJarLauncher";
 
-	private static final String DEFAULT_LIBRARY = "org.springframework.boot.experimental:spring-boot-thin-launcher:jar:exec:1.0.22.BUILD-SNAPSHOT";
+	private static final String DEFAULT_LIBRARY = "org.springframework.boot.experimental:spring-boot-thin-launcher:jar:exec:1.0.22.RELEASE";
 
 	private Properties properties;
 
