32c48fecbaad41a5c7cc270915bc38de09975dd7
Dave Syer <dsyer@vmware.com>
2025-01-15 11:08:19 +0000

    Fix build

--- gradle-plugin/src/main/java/org/springframework/boot/experimental/gradle/PropertiesTask.java
+++ gradle-plugin/src/main/java/org/springframework/boot/experimental/gradle/PropertiesTask.java
@@ -28,7 +28,6 @@ import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.OutputFile;
 import org.gradle.api.tasks.TaskAction;
 import org.gradle.api.tasks.TaskExecutionException;
-
 import org.springframework.util.StringUtils;
 
 /**
@@ -145,7 +144,6 @@ public class PropertiesTask extends DefaultTask {
 	/**
 	 * The name of the thin properties file (defaults to "thin").
 	 */
-	@Override
 	public void setName(String name) {
 		this.name = name;
 	}
